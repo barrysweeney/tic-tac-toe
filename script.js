2bd2b8e870da5ed1cf46a59d22d9e9f0256a2ff1
barrysweeney <barrysweeney172@live.co.uk>
2020-03-19 11:48:36 +0000

    refactor

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
-// player factory
-const Player = (name, marker) => {
-  const getName = () => name;
-  const getMarker = () => marker;
-
-  return { getName, getMarker };
-};
-
 // gameboard module using revealing module pattern
 const gameboard = (function() {
   "use strict";
 
-  const tilesCollection = document.getElementsByClassName("tile");
-  const tiles = Array.from(tilesCollection);
+  const board = Array.from(document.getElementsByClassName("tile"));
 
-  function makeSelectable() {
-    // only selects tile if not already selected
-    for (let i = 0; i < tiles.length; i++) {
-      tiles[i].addEventListener("click", function() {
-        game.selectTile(tiles[i]);
+  function makeTilesSelectable() {
+    for (let i = 0; i < board.length; i++) {
+      board[i].addEventListener("click", function() {
+        game.playerSelection(board[i]);
       });
     }
   }
 
-  return { tiles, makeSelectable };
+  return { board, makeTilesSelectable };
 })();
 
 // game module using revealing module pattern
@@ -30,28 +20,52 @@ const game = (function() {
   "use strict";
 
   let turnDisplay = document.getElementById("turn");
-  let turn;
+  let gameDisplay = document.getElementById("gameDisplay");
+  let playerForm = document.getElementById("playerForm");
+  let playerTwoForm = document.getElementById("playerTwoForm");
+  let singlePlayerModeButton = document.getElementById("singlePlay");
+  let twoPlayerModeButton = document.getElementById("twoPlay");
+  let startButton = document.getElementById("start");
+  let resetButton = document.getElementById("reset");
+
+  singlePlayerModeButton.addEventListener("click", setupSinglePlayerMode);
+  twoPlayerModeButton.addEventListener("click", setupTwoPlayerMode);
+  startButton.addEventListener("click", startGame);
+  resetButton.addEventListener("click", reset);
+
+  let whoseTurn;
   let player1;
   let player2;
-  let playerForm = document.getElementById("playerForm");
-  let gameDisplay = document.getElementById("gameDisplay");
-  let playerTwoForm = document.getElementById("playerTwoForm");
+  let twoPlayerMode;
+  let singlePlayerMode;
 
-  function displaySinglePlayerMode() {
+  function setupSinglePlayerMode() {
     gameDisplay.style.display = "block";
-    playerTwoForm.style.display = "none";
-    twoPlayerModeButton.style.display = "none";
+    playerTwoForm.style.display = "none"; // hide second player form
+    singlePlayerMode = true;
+    twoPlayerMode = false;
+    twoPlayerModeButton.style.display = "none"; // hide two player mode button
   }
 
-  function displayTwoPlayerMode() {
+  function setupTwoPlayerMode() {
     gameDisplay.style.display = "block";
-    playerTwoForm.style.display = "block";
-    singlePlayerModeButton.style.display = "none";
+    playerTwoForm.style.display = "block"; // display second player form
+    singlePlayerMode = false;
+    twoPlayerMode = true;
+    singlePlayerModeButton.style.display = "none"; // hide single player mode button
   }
 
-  // checks if values enterred in all form fields
-  function formComplete() {
-    if (playerTwoForm.style.display === "block") {
+  function startGame() {
+    if (isFormComplete()) {
+      createPlayers();
+      whoseTurn = player1; // arbitrary default choice for who has first turn
+      turnDisplay.innerHTML = `${whoseTurn.getName()}'s turn`;
+      gameboard.makeTilesSelectable();
+    }
+  }
+
+  function isFormComplete() {
+    if (twoPlayerMode) {
       return (
         document.getElementById("inputPlayer1").value.length > 0 &&
         document.getElementById("inputPlayer2").value.length > 0
@@ -61,92 +75,57 @@ const game = (function() {
     }
   }
 
+  function createPlayers() {
+    if (singlePlayerMode) {
+      let { nameOfPlayer1 } = getFormValues();
+      player1 = Player(nameOfPlayer1, "x");
+      player2 = Player("Computer", "o");
+    }
+    if (twoPlayerMode) {
+      let { nameOfPlayer1, nameOfPlayer2 } = getFormValues();
+      player1 = Player(nameOfPlayer1, "x");
+      player2 = Player(nameOfPlayer2, "o");
+    }
+  }
+
   function getFormValues() {
-    if (playerTwoForm.style.display === "block") {
-      let name1 = document.getElementById("inputPlayer1").value;
-      let name2 = document.getElementById("inputPlayer2").value;
-      return { name1, name2 };
-    } else {
-      let name1 = document.getElementById("inputPlayer1").value;
-      return { name1 };
+    if (singlePlayerMode) {
+      let nameOfPlayer1 = document.getElementById("inputPlayer1").value;
+      return { nameOfPlayer1 };
+    }
+    if (twoPlayerMode) {
+      let nameOfPlayer1 = document.getElementById("inputPlayer1").value;
+      let nameOfPlayer2 = document.getElementById("inputPlayer2").value;
+      return { nameOfPlayer1, nameOfPlayer2 };
     }
   }
 
-  function startGame() {
-    if (formComplete()) {
-      if (playerTwoForm.style.display === "block") {
-        let { name1, name2 } = getFormValues();
-        player1 = Player(name1, "x");
-        player2 = Player(name2, "o");
+  let playerSelection = tile => {
+    if (tile.innerHTML === "") {
+      let p = document.createElement("p");
+      p.innerHTML = getTurnMarker();
+      tile.appendChild(p);
+      if (checkIfWon() === true) {
+        showWinner();
+        finishGame();
+      } else if (checkIfTie() === true) {
+        showTie();
+        finishGame();
       } else {
-        let { name1 } = getFormValues();
-        player1 = Player(name1, "x");
-        player2 = Player("computer", "o");
-      }
-      turn = player1;
-      turnDisplay.innerHTML = `${turn.getName()}'s turn`;
-      gameboard.makeSelectable();
-    }
-  }
-
-  let startButton = document.getElementById("start");
-  let resetButton = document.getElementById("reset");
-  let singlePlayerModeButton = document.getElementById("singlePlay");
-  let twoPlayerModeButton = document.getElementById("twoPlay");
-
-  singlePlayerModeButton.addEventListener("click", displaySinglePlayerMode);
-  twoPlayerModeButton.addEventListener("click", displayTwoPlayerMode);
-
-  startButton.addEventListener("click", startGame);
-
-  const winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  let reset = () => {
-    for (let i = 0; i < gameboard.tiles.length; i++) {
-      gameboard.tiles[i].innerHTML = "";
-    }
-    turnDisplay.innerHTML = "";
-  };
-
-  let finishGame = () => {
-    for (let i = 0; i < gameboard.tiles.length; i++) {
-      if (gameboard.tiles[i].innerHTML === "") {
-        gameboard.tiles[i].innerHTML = " ";
+        nextTurn();
+        if (singlePlayerMode) {
+          computerSelection();
+        }
       }
     }
-  };
-
-  resetButton.addEventListener("click", reset);
-
-  let checkIfTie = () => {
-    let tie = true;
-    for (let i = 0; i < gameboard.tiles.length; i++) {
-      if (gameboard.tiles[i].innerHTML === "") {
-        tie = false;
-      }
-    }
-    return tie;
-  };
-
-  let generateRandomIndex = () => {
-    return Math.floor(Math.random() * gameboard.tiles.length);
   };
 
   let computerSelection = () => {
     let index = generateRandomIndex();
-    if (gameboard.tiles[index].innerHTML === "") {
+    if (gameboard.board[index].innerHTML === "") {
       let p = document.createElement("p");
       p.innerHTML = getTurnMarker();
-      gameboard.tiles[index].appendChild(p);
+      gameboard.board[index].appendChild(p);
       if (checkIfWon() === true) {
         showWinner();
         finishGame();
@@ -161,32 +140,18 @@ const game = (function() {
     }
   };
 
-  let selectTile = tile => {
-    if (tile.innerHTML === "") {
-      let p = document.createElement("p");
-      p.innerHTML = getTurnMarker();
-      tile.appendChild(p);
-      if (checkIfWon() === true) {
-        showWinner();
-        finishGame();
-      } else if (checkIfTie() === true) {
-        showTie();
-        finishGame();
-      } else {
-        nextTurn();
-        if (playerTwoForm.style.display === "none") {
-          computerSelection();
-        }
+  const getTurnMarker = () => whoseTurn.getMarker();
+
+  const nextTurn = () => {
+    if (whoseTurn === player1) {
+      whoseTurn = player2;
+      if (twoPlayerMode) {
+        turnDisplay.innerHTML = `${whoseTurn.getName()}'s turn`;
       }
+    } else {
+      whoseTurn = player1;
+      turnDisplay.innerHTML = `${whoseTurn.getName()}'s turn`;
     }
-  };
-
-  let showTie = () => {
-    turnDisplay.innerHTML = "It's a tie";
-  };
-
-  let showWinner = () => {
-    turnDisplay.innerHTML = `${turn.getName()} wins`;
   };
 
   let checkIfWon = () => {
@@ -198,18 +163,18 @@ const game = (function() {
       secondTile = winCombos[i][1];
       thirdTile = winCombos[i][2];
 
-      // only check if all three tiles are marked
+      // only check if all three board are marked
       if (
-        gameboard.tiles[firstTile].innerHTML !== "" &&
-        gameboard.tiles[secondTile].innerHTML !== "" &&
-        gameboard.tiles[thirdTile].innerHTML !== ""
+        gameboard.board[firstTile].innerHTML !== "" &&
+        gameboard.board[secondTile].innerHTML !== "" &&
+        gameboard.board[thirdTile].innerHTML !== ""
       ) {
-        // check that the mark is the same on all three tiles
+        // check that the marker is the same on all three board
         if (
-          gameboard.tiles[firstTile].innerHTML ===
-            gameboard.tiles[secondTile].innerHTML &&
-          gameboard.tiles[secondTile].innerHTML ===
-            gameboard.tiles[thirdTile].innerHTML
+          gameboard.board[firstTile].innerHTML ===
+            gameboard.board[secondTile].innerHTML &&
+          gameboard.board[secondTile].innerHTML ===
+            gameboard.board[thirdTile].innerHTML
         ) {
           return true;
         }
@@ -217,19 +182,67 @@ const game = (function() {
     }
   };
 
-  const getTurnMarker = () => turn.getMarker();
+  let showWinner = () => {
+    turnDisplay.innerHTML = `${whoseTurn.getName()} wins`;
+  };
 
-  const nextTurn = () => {
-    if (turn === player1) {
-      turn = player2;
-      if (playerTwoForm.style.display === "block") {
-        turnDisplay.innerHTML = `${turn.getName()}'s turn`;
+  let checkIfTie = () => {
+    let tie = true;
+    for (let i = 0; i < gameboard.board.length; i++) {
+      if (gameboard.board[i].innerHTML === "") {
+        tie = false;
       }
-    } else {
-      turn = player1;
-      turnDisplay.innerHTML = `${turn.getName()}'s turn`;
+    }
+    return tie;
+  };
+
+  let showTie = () => {
+    turnDisplay.innerHTML = "It's a tie";
+  };
+
+  let finishGame = () => {
+    for (let i = 0; i < gameboard.board.length; i++) {
+      if (gameboard.board[i].innerHTML === "") {
+        gameboard.board[i].innerHTML = " ";
+      }
     }
   };
 
-  return { selectTile, generateRandomIndex };
+  function reset() {
+    // set board back to intial state
+    for (let i = 0; i < gameboard.board.length; i++) {
+      gameboard.board[i].innerHTML = "";
+    }
+    // set page back to initial state
+    singlePlayerModeButton.style.display = "initial";
+    twoPlayerModeButton.style.display = "initial";
+    turnDisplay.innerHTML = "";
+    gameDisplay.style.display = "none";
+    playerForm.reset();
+  }
+
+  let generateRandomIndex = () => {
+    return Math.floor(Math.random() * gameboard.board.length);
+  };
+
+  const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  return { playerSelection };
 })();
+
+// player factory
+const Player = (name, marker) => {
+  const getName = () => name;
+  const getMarker = () => marker;
+
+  return { getName, getMarker };
+};
